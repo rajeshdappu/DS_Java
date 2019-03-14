42136dcc4f35ce76746ee91312b5fdac044a10ca
RajeshDappu <rajesh.d@mtwlabs.com>
2019-03-14 20:58:27 +0530

    unweightedshortestpaths added

--- graph.java
+++ graph.java
@@ -5,16 +5,18 @@ class graph{
         System.out.println("Hello");
         char nodes[] = {'A','B','C','D','E'}; 
         int a[][]  = { //adjacency matrix of graph nodes A,B,C,D,E
-            {1,0,1,0,0},
-            {0,1,0,1,0},
-            {0,0,1,0,0},
-            {0,0,0,1,0},
-            {0,0,0,0,1}
+            {1,1,0,0,1},
+            {0,1,1,0,0},
+            {0,0,1,1,0},
+            {1,0,0,1,0},
+            {0,0,1,0,1}
         };
 
         // DFSTraversal(nodes,a);
         // BFSTraversal(nodes,a);
-        topologicalSort(a,nodes);
+        // topologicalSort(a,nodes);
+
+        unweightedShortestPaths(a,nodes,0);
 
     }
 
@@ -101,6 +103,65 @@ class graph{
         }
         for(int i=0;i<nodes.length;i++){
             System.out.println("Topological order of  : "+nodes[i] +" - "+ topologicalOrder[i]);
+        }
+    }
+
+    static void unweightedShortestPaths(int a[][],char nodes[],int fromNodeIndex){
+        int[] distance = new int[nodes.length];
+        int[] path = new int[nodes.length];
+        for(int i=0;i<nodes.length;i++)distance[i]=-1;
+        distance[fromNodeIndex]=0;
+        Queue<Integer> queue = new LinkedList<Integer>();
+        queue.add(fromNodeIndex);
+        while(queue.size()>0){
+            int node = queue.remove();
+            for(int i=0;i<nodes.length;i++){
+                if(a[node][i] == 1){
+                    if(distance[i] == -1){
+                        distance[i] = distance[node]+1;
+                        path[i] = node;
+                        queue.add(i);
+                    }
+                }
+            }
+        }
+
+        for(int i=0;i<nodes.length;i++){
+            System.out.println("Path of  : "+nodes[i] +" - "+ nodes[path[i]]);
+        }
+
+         for(int i=0;i<nodes.length;i++){
+            System.out.println("Distance of  : "+nodes[i] +" - "+ distance[i]);
+        }
+    }
+
+
+      static void unweightedShortestPaths(int a[][],char nodes[],int fromNodeIndex){
+        int[] distance = new int[nodes.length];
+        int[] path = new int[nodes.length];
+        for(int i=0;i<nodes.length;i++)distance[i]=-1;
+        distance[fromNodeIndex]=0;
+        Queue<Integer> queue = new LinkedList<Integer>();
+        queue.add(fromNodeIndex);
+        while(queue.size()>0){
+            int node = queue.remove();
+            for(int i=0;i<nodes.length;i++){
+                if(a[node][i] == 1){
+                    if(distance[i] == -1){
+                        distance[i] = distance[node]+1;
+                        path[i] = node;
+                        queue.add(i);
+                    }
+                }
+            }
+        }
+
+        for(int i=0;i<nodes.length;i++){
+            System.out.println("Path of  : "+nodes[i] +" - "+ nodes[path[i]]);
+        }
+
+         for(int i=0;i<nodes.length;i++){
+            System.out.println("Distance of  : "+nodes[i] +" - "+ distance[i]);
         }
     }
 }
